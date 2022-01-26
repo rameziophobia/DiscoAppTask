2dd7c168a778622857efd3735f4567e0107770a4
Ramez <rameznoshy@gmail.com>
2022-01-27 00:03:34 +0200

    feat: implement file converter using bridge pattern

--- backend/file_converter.py
+++ backend/file_converter.py
@@ -1,9 +1,54 @@
+from abc import ABC, abstractmethod
+import pandas as pd
+
+
+class BaseFileLoader(ABC):
+    @abstractmethod
+    def load(self, file):
+        pass
+
+
+class BaseFileConverter(ABC):
+    @abstractmethod
+    def convert(self, file):
+        pass
+
+
+class JsonConverter(BaseFileConverter):
+    def convert(self, df):
+        return df.to_json(orient='records')
+
+
+class CsvFileLoader(BaseFileLoader):
+    def load(self, file):
+        return pd.read_csv(file)
+
+class XlsxFileLoader(BaseFileLoader):
+    def load(self, file):
+        return pd.read_excel(file)
+
+
+class XmlFileLoader(BaseFileLoader):
+    def load(self, file):
+        return pd.read_xml(file)
+
+
 class FileConverter():
-  def __init__(self, fromType, toType, file):
-    self.fromType = fromType
-    self.toType = toType
-    self.file = file
+    # Bridge pattern is used for the converter, data loader
+    def __init__(self, fromType, toType):
+        self.dataLoader = None
+        self.converter = None
+        if fromType == 'csv':
+            self.dataLoader = CsvFileLoader()
+        if fromType == 'xml':
+            self.dataLoader = XlsxFileLoader()
+        if fromType == 'xlsx':
+            self.dataLoader = XmlFileLoader()
+        if toType == 'json':
+            self.converter = JsonConverter()
 
-  def convert(self):
-    raise NotImplementedError()
-
+    def convert(self, file):
+        if self.dataLoader is None or self.converter is None:
+            raise NotImplementedError()
+        data = self.dataLoader.load(file)
+        return self.converter.convert(data)
