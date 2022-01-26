225e37184315721aa57033b900f8ebb1358ac7f1
Ramez <rameznoshy@gmail.com>
2022-01-26 21:43:21 +0200

    feat: basic backend for exact search done (feature 1)

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,26 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import csv
+import os
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
+DATA_PATH = os.getenv('IMDB_DATA_PATH')
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+def do_args_allow_row(row, args):
+    return all([row[arg] == arg_value for  arg, arg_value in args.items()])
+
+@app.route('/search', methods=['GET'])
+def search():
+    res = []
+    args = request.args
+
+    with open(DATA_PATH, encoding="utf8") as csv_file:
+        data = csv.DictReader(csv_file)
+        res = [row for row in data if do_args_allow_row(row, args)]
+
+    # id, title, release_date, overview, popularity, vote_average, vote_count, video
+    return jsonify(res)
