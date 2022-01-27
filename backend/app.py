90d5b9cb2f6b27597ebd8ac6ff78b5a856811ebf
Ramez <rameznoshy@gmail.com>
2022-01-27 02:12:14 +0200

    fix: sort overview_simple results

--- backend/app.py
+++ backend/app.py
@@ -61,7 +61,7 @@ class Search(Resource):
             data = csv.DictReader(csv_file)
             try:
                 res = [row for row in data if do_args_allow_row(row, args)]
-                if 'overview' in args:
+                if 'overview' in args or 'overview_simple':
                     res.sort(key=lambda row: count_total_words_in_ref(
                         row['overview'], args['overview']), reverse=True)
             except:
